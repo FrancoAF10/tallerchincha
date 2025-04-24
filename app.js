c968fd1da248c6f8fd53ee121341241f22be9815
FrancoAF10 <antonfelixfranco@gmail.com>
2025-04-24 09:47:08 -0500

    proceso listar

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname,'public')))
 
+
+//motor de plantillas
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'))
 //configuración rutas
 app.use('/',rutaVehiculo) //principal
 //app.use('/api/marcas',rutaMarca)//suministro de datos
--- routes/vehiculos.js
+++ routes/vehiculos.js
@@ -3,8 +3,21 @@ const router =express.Router()
 const db=require('../config/database')
 
 router.get('/',async(req,res)=>{
-  const [vehiculos]=await db.query(`SELECT * FROM VEHICULOS`)
-  res.send(vehiculos)
+
+  try{
+    const query=` SELECT V.idvehiculo,
+      M.marca,
+      V.modelo,
+      V.color,
+      V.combustible,
+      V.afabricacion,
+      V.condicion FROM vehiculos V INNER JOIN marcas M ON V.idmarca=M.idmarca`;
+    const [vehiculos]=await db.query(query)
+        //res.send(vehiculos)
+    res.render('index',{vehiculos})
+  }catch(error){
+    console.error(error)
+  }
 })
 
 module.exports=router
